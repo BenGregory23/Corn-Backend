9dedb1abb545ba1e541ccd456e91b812b47c4db0
BenGregory23 <bengregorygbr@gmail.com>
2023-10-01 15:51:58 +0200

    Added tests

---
+++ test/repository/authRepo.test.ts
@@ -0,0 +1,61 @@
+import { assert, expect, should, test } from "vitest";
+import { ObjectId } from "mongodb";
+import { connectDB } from "../../src/db";
+
+import {register, login} from '../../src/repo/authRepo';
+import { deleteUser } from "../../src/repo/userRepo";
+
+
+await connectDB();
+
+const user = {
+    name: "test",
+    email: Math.random().toString(36).substring(7) + "@test.com",
+    password: "test",
+    genres: [],
+    friends: [],
+    movies: [],
+}   
+
+let userId: string;
+
+
+test("should register a user and return an id", async () => {
+    const userResult = await register(user);
+    assert(userResult != null);
+    userId = userResult;
+})
+
+test("should not register a user if email already exists", async () => {
+    try{
+        await register(user);
+        assert(false);
+    } catch(err){
+        assert(true);
+    }
+})
+
+
+test("should login a user and return a user", async () => {
+    const userResult = await login(user.email, user.password);
+    assert(user != null);
+})
+
+test("should delete a user", async () => {
+    const result = await deleteUser(userId);
+    
+    assert(result);
+})
+
+
+
+test("should return null if user does not exist", async () => {
+    const userResult = await login("test", "test");
+    assert(userResult == null);
+})
+
+test("should return null if password is incorrect", async () => {
+    const userResult = await login(user.email, "NotTest");
+    assert(userResult == null);
+})
+
---
+++ test/repository/userRepo.test.ts
@@ -0,0 +1,69 @@
+import { assert, expect, test } from "vitest";
+import { ObjectId } from "mongodb";
+import {getUsers, getUser, getFriends, getGroups, getMoviesFromUser} from '../../src/repo/userRepo';
+import { connectDB } from "../../src/db";
+
+// Since we are not starting the server, we need to connect to the DB
+await connectDB();
+
+test("should return users", async () => {
+   
+    const users = await getUsers();
+    assert(users.length > 0);
+});
+
+test("should return a user", async () => {
+   
+    // get users then pick the first and get it
+    const users = await getUsers();
+    const user = await getUser(users[0]._id);
+    assert(user);
+})
+
+test("should return null if user does not exist", async () => {
+       
+    // get users then pick the first and get it
+    const user = await getUser(new ObjectId());
+    assert(user == null);
+})
+
+
+test.todo("should update a user")
+test.todo("should delete a user")
+
+
+
+test("should return friends", async () => {
+    // get users then pick the first and get it
+    const users = await getUsers();
+    const friends = await getFriends(users[0]._id);
+    // might not have friends
+    // TODO find a better way to test this
+    assert(friends.length > 0);
+})
+
+test.todo("should add a friend to a user")
+test.todo("should remove a friend from a user")
+
+
+test("should return groups", async () => {
+    // get users then pick the first and get it
+    const users = await getUsers();
+    const groups = await getGroups(users[0]._id);
+    
+    assert(groups != null);
+})
+
+test("should return movies from a user", async () => {
+    // get users then pick the first and get it
+    const users = await getUsers();
+    const movies = await getMoviesFromUser(users[0]._id);
+    
+    assert(movies != null);
+})
+
+
+
+
+
+
